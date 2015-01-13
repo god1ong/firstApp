d0dfb515b39d532a022fedf41b4f06cd164971bc
godlong <godlong@godlongRMBP.local>
2015-01-13 10:40:49 +0800

    Initial Commit

---
+++ firstApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  firstApp
+//
+//  Created by godlong on 15-1-13.
+//  Copyright (c) 2015年 godlong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ firstApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  firstApp
+//
+//  Created by godlong on 15-1-13.
+//  Copyright (c) 2015年 godlong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
